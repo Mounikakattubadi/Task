ccabe249832b24afb64132255bb236d0a5bb05a6
GannuNb <nbganesh2002@gmail.com>
2025-09-24 22:54:20 +0530

    5

--- src/Hero.js
+++ src/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
       }}
     >
       <div className="hero__content">
-        <h1 className="hero__title">Best Weight Loss Injections 2025</h1>
+        <h1 className="hero__title">Best Weight Loss Injections of 2025</h1>
         <p className="hero__desc">
           Tired of overeating? Compare top weight loss brands to find the best
           fit for your unique needs. Explore treatment plans, such as{" "}
@@ -23,7 +23,7 @@ const Hero = () => {
           and kickstart a healthier you.
         </p>    
         <p className="hero__updated">
-          <span className="dot"></span> Last Updated: September 2025
+<span className="tick"></span> Last Updated: September 2025
         </p>
       </div>
     </section>
